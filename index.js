90feced1eeee67a71e6bf4fb3366bec110f83103
Tanish kumar <81621114+UnknonPerson@users.noreply.github.com>
2026-01-18 02:03:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let btn_no = document.getElementById("no");
+
+btn_no.addEventListener('click',() => {
+    let x=Math.floor(Math.random()*300-200);
+    let y=Math.floor(Math.random()*300-200);
+    console.log(`translate(${x},${y})`);
+    btn_no.style.transform=`translate(${x}px,${y}px)`;
+})
+
+let btn_yes = document.getElementById('yes');
+
+btn_yes.addEventListener('click', () => {
+
+    document.getElementsByClassName('ONE')[0].style.display='none';
+    document.getElementsByClassName('TWO')[0].style.display="flex";
+
+    setTimeout(() => {
+        document.getElementsByClassName('TWO')[0].style.display="none";
+        document.getElementsByClassName('THREE')[0].style.display="flex";
+    },2000)
+
+});
